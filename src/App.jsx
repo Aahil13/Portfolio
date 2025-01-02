cd5a95e45809f7737c244498f47b46ed1815587a
Aahil13 <onyeanunaprince@gmail.com>
2025-01-02 20:27:42 +0100

    Update import for Companies

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/sections/Hero'
 import Services from './components/sections/Services'
 import Projects from './components/sections/Projects'
-import Companies from './components/sections/companies'
+import Companies from './components/sections/Companies'
 import Contact from './components/sections/Contact'
 import ProfileShowcase from './components/sections/profiles'
 
